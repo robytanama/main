95db504a10d9b898984ddcd7f7e06ef68ad0a7e1
robytanama <tanamaroby@gmail.com>
2019-09-19 17:46:03 +0800

    Deleted Address from the program

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -12,7 +12,7 @@ import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
 /**
- * A utility class to help with building Person objects.
+ * A utility class to help with building Person object.
  */
 public class PersonBuilder {
 
